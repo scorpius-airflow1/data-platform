00b25bb4854ba942c3f1b121e37375b1a4b906f8
echandia27 <jp.echandia@gmail.com>
2026-06-05 21:14:29 -0500

    fix: set orchestrator to manual schedule to avoid DB locks

--- pipelines/dags/orchestrator.py
+++ pipelines/dags/orchestrator.py
@@ -5,7 +5,7 @@ from datetime import datetime
 @dag(
     dag_id="master_orchestrator",
     start_date=datetime(2024, 1, 1),
-    schedule="@daily",
+    schedule=None,
     catchup=False,
     tags=["orchestrator", "master"],
 )
